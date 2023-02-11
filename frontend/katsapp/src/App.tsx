99562bb2bad586ea9f6a3e91dd2c2c6f67909bd8
julesd7 <jules.dufraiche@epitech.eu>
2023-02-11 18:42:31 +0100

    add front

--- frontend/katsapp/src/App.tsx
+++ frontend/katsapp/src/App.tsx
@@ -2,15 +2,6 @@ import { Center, VStack } from '@chakra-ui/react';
 // import Welcome from './pages/Welcome';
 import { default as Routes } from './Routes';
 
-function App() {
-  return (
-    <Center mt="160px">
-		<VStack spacing="32px">
-			<Routes />
-			{/* <Welcome /> */}
-		</VStack>
-	</Center>
-  );
-}
+const App = () => <Routes />;
 
 export default App;
--- frontend/katsapp/src/Routes.tsx
+++ frontend/katsapp/src/Routes.tsx
@@ -1,6 +1,7 @@
+import { BrowserRouter, Route, Routes as RouterRoute } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-import { BrowserRouter, Route, Routes as RouterRoute } from 'react-router-dom';
 import { default as Welcome } from './pages/Welcome';
 
 export default function Routes() {
@@ -10,6 +11,7 @@ export default function Routes() {
 				<Route path="/" element={<Welcome />} />
 				<Route path="/signup" element={<SignUp />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/home" element={<Home />} />
 			</RouterRoute>
 		</BrowserRouter>
 	);
